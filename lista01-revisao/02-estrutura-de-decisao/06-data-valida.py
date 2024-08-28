3cc8d8b9ef5c007843e8020dec2db99e55a29a3e
Adelino Martins <adelino.developer@gmail.com>
2024-08-27 21:15:52 -0300

    feat: add Lista de Exercício 00 (Revisão) - Estrutura de Repetição [questão 6]

--- lista01-revisao/02-estrutura-de-decisao/06-data-valida.py
+++ lista01-revisao/02-estrutura-de-decisao/06-data-valida.py
@@ -1,1 +1,36 @@
 data = input("Digite uma data (no formato dd/mm/aaaa):")
+
+dia = int(data[0:2])
+mes = int(data[3:5])
+ano = int(data[6::])
+
+eh_data_valida = True
+
+# Analisa se a data é válida
+if mes > 12 or mes < 1:
+    eh_data_valida = False
+
+elif dia > 31 or dia < 1:
+    eh_data_valida = False
+
+elif mes in (2, 4, 6, 9, 11) and dia > 30:
+    eh_data_valida = False
+
+elif mes == 2 and dia > 29:
+    eh_data_valida = False
+
+elif mes == 2:
+    # Verifica se é ano bissexto
+    eh_bissexto = ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0)
+    
+    if eh_bissexto and dia > 29:
+        eh_data_valida = False
+    elif not eh_bissexto and dia > 28:
+        eh_data_valida = False
+
+
+#Mostra se a data é válida ou não:
+if eh_data_valida:
+    print("A data é válida.")
+else:
+    print("A data NÃO é válida.")
