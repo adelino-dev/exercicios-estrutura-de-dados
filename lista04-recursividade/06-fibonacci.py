38bf92a2d2ac1ca69268dfebd5697f329b5c464f
Adelino Martins <90815143+adelino-dev@users.noreply.github.com>
2024-09-03 22:32:16 +0000

    feat: add Lista de Exercícios 03 (Recursividade) - questão 06 [Fibonacci]

---
+++ lista04-recursividade/06-fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+if __name__ == "__main__":
+    num = int(input("Número:"))
+    print(fibonacci(num))
